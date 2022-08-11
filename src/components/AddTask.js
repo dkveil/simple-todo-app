18ee9a52023254c3f2c66d079b2f696e55d5a641
Damian Kądziela <dkkquality@gmail.com>
2022-08-11 15:54:53 +0200

    AddTask items

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,7 +1,61 @@
+import React from 'react'
+import styled from 'styled-components'
+
 const AddTask = () => {
+
+    const minDate = new Date().toISOString().slice(0, 10);
+
+    const [task, setTask] = React.useState({
+        name: '',
+        important: false,
+        date: new Date().toISOString().slice(0,10)
+    })
+
+    const handleChange = e => {
+        if(e.target.type === 'checkbox'){
+            setTask(prev => ({
+                ...prev,
+                [e.target.name]: e.target.checked
+            }))
+        } else {
+            setTask(prev => ({
+                ...prev,
+                [e.target.name]: e.target.value
+            }))
+        }
+    }
+
     return (
-        <>
-        </>
+        <div>
+            <div>
+                <input
+                    name="name"
+                    type="text"
+                    placeholder="Add task name"
+                    value={task.name}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label htmlFor="important">is important?</label>
+                <input
+                    name="important"
+                    type="checkbox"
+                    checked={task.important}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label htmlFor="date">deadline:</label>
+                <input
+                    name="date"
+                    value={task.date}
+                    type="date"
+                    min={minDate}
+                    onChange={handleChange}
+                />
+            </div>
+        </div>
     );
 }
 
